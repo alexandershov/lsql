306523326e21b2db3439fbd0284bfc06aba0470a
aershov <codumentary.com@gmail.com>
2015-09-25 21:11:05 +0300

    Add test helper
    assert_query.

--- tests/test_lsql.py
+++ tests/test_lsql.py
@@ -2,15 +2,22 @@ import os
 
 import lsql
 
-
 DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def test_simple():
-    names = list(lsql.run_query("SELECT name FROM {} ORDER BY name".format(DIR)))
-    assert names == [['README.md'], ['small.py']]
+    assert_query([['README.md'], ['small.py']],
+                 'SELECT name', 'ORDER BY name')
 
 
 def test_where():
-    names = list(lsql.run_query("SELECT name FROM {} WHERE extension = 'py' ORDER BY name".format(DIR)))
-    assert names == [['small.py']]
+    assert_query([['small.py']],
+                 'SELECT name', "WHERE extension = 'py' ORDER BY name")
+
+
+def assert_query(expected_results,
+                 before_from,
+                 after_from='',
+                 from_clause='FROM {}'.format(DIR)):
+    results = list(lsql.run_query(' '.join([before_from, from_clause, after_from])))
+    assert results == expected_results
