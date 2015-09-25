5be0dbd2632eb2fff6e41a786253e0f8e54c2096
aershov <codumentary.com@gmail.com>
2015-09-25 21:14:43 +0300

    Test unordered query

--- tests/test_lsql.py
+++ tests/test_lsql.py
@@ -3,9 +3,15 @@ import os
 import lsql
 
 DIR = os.path.join(os.path.dirname(__file__), 'data')
+FROM_CLAUSE = 'FROM {}'.format(DIR)
 
 
 def test_simple():
+    results = get_results('SELECT name')
+    assert_same_items([['README.md'], ['small.py']], results)
+
+
+def test_order():
     assert_query([['README.md'], ['small.py']],
                  'SELECT name', 'ORDER BY name')
 
@@ -18,6 +24,14 @@ def test_where():
 def assert_query(expected_results,
                  before_from,
                  after_from='',
-                 from_clause='FROM {}'.format(DIR)):
-    results = list(lsql.run_query(' '.join([before_from, from_clause, after_from])))
+                 from_clause=FROM_CLAUSE):
+    results = get_results(before_from, after_from, from_clause)
     assert results == expected_results
+
+
+def get_results(before_from, after_from='', from_clause=FROM_CLAUSE):
+    return list(lsql.run_query(' '.join([before_from, from_clause, after_from])))
+
+
+def assert_same_items(seq_x, seq_y):
+    assert sorted(seq_x) == sorted(seq_y)
