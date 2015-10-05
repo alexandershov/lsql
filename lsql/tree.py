f243af1980ff0608e42890aed603a98646355d3f
aershov <codumentary.com@gmail.com>
2015-10-05 10:13:59 +0300

    Add parse tree nodes

---
+++ lsql/tree.py
@@ -0,0 +1,95 @@
+from __future__ import division, print_function
+
+from functools import wraps
+import operator
+import re
+
+
+def sql_function(fn):
+    @wraps(fn)
+    def wrapped(x, y):
+        if x is None or y is None:
+            return None
+        return fn(x, y)
+
+    return wrapped
+
+
+def like(string, pattern):
+    pattern = re.escape(pattern)
+    pattern = pattern.replace(r'\%', '.*').replace(r'\_', '.')
+    return rlike(string, pattern)
+
+
+def rlike(string, re_pattern):
+    # we need re.DOTALL because string can contain newlines (e.g in 'content' column)
+    regex = re.compile(re_pattern + '$', re.DOTALL)
+    if not isinstance(string, list):
+        string = [string]
+    return any(regex.match(line) for line in string)
+
+
+FUNCTIONS = {
+    '<>': sql_function(operator.ne),
+    '!=': sql_function(operator.ne),
+    '=': sql_function(operator.eq),
+    '==': sql_function(operator.eq),
+    '<': sql_function(operator.lt),
+    '<=': sql_function(operator.le),
+    '>': sql_function(operator.gt),
+    '>=': sql_function(operator.ge),
+    'like': sql_function(like),
+    'rlike': sql_function(rlike),
+    'lower': sql_function(lambda s: s.lower()),
+    'upper': sql_function(lambda s: s.upper()),
+    'length': sql_function(len),
+    # 'age': sql_function(age),  TODO
+    # 'btrim': sql_function(btrim), TODO
+}
+
+
+class Node(object):
+    def eval(self, context):
+        raise NotImplementedError
+
+
+class LiteralNode(Node):
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self, context):
+        return self.value
+
+
+class FunctionNode(Node):
+    def __init__(self, function, args):
+        self.function = function
+        self.args = args
+
+    def eval(self, context):
+        args = [arg.eval(context) for arg in self.args]
+        return self.function(args)
+
+
+class OrNode(Node):
+    def __init__(self, args):
+        self.args = args
+
+    def eval(self, context):
+        return any(arg.eval(context) for arg in self.args)
+
+
+class AndNode(Node):
+    def __init__(self, args):
+        self.args = args
+
+    def eval(self, context):
+        return all(arg.eval(context) for arg in self.args)
+
+
+class NameNode(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def eval(self, context):
+        return context.get_value(self.name)
