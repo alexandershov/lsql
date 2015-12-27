cecf1a040185403e02b9457929de6230c0118b0f
aershov <codumentary.com@gmail.com>
2015-12-27 23:07:26 +0300

    Add parser stub

--- lsql/parser.py
+++ lsql/parser.py
@@ -2,9 +2,37 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from lsql import expr
 
+
 class Parser(object):
     def __init__(self, tokens):
-        self.tokens = tokens
+        self._tokens = list(tokens)
+        self._index = 0
+
+    def advance(self):
+        self._check_bounds(self._index + 1)
+        self._index += 1
+
+    def peek(self):
+        return self._tokens[self._index + 1]
+
+    @property
+    def token(self):
+        return self._tokens[self._index]
 
     def parse(self):
-        pass
+        self.advance()
+        return self.expr(left_bp=0)
+
+    def expr(self, left_bp=0):
+        token = self.token
+        self.advance()
+        value = token.prefix(self)
+        while self.token.right_bp > left_bp:
+            token = self.token
+            self.advance()
+            value = token.suffix(value, self)
+        return value
+
+    def _check_bounds(self, index):
+        if index < 0 or index >= len(self._tokens):
+            raise IndexError(index, len(self._tokens))
